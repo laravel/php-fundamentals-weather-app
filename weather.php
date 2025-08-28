08d200aca2826f24f559a1ef9965b8848178e7a3
Christoph Rumpel <christoph@christoph-rumpel.com>
2025-08-28 09:51:35 +0200

    Add php executable

---
+++ weather.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+use WeatherApp\WeatherService;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$weatherService = new WeatherService();
+$city = 'Vienna';
+
+echo "Getting weather for $city...\n";
+$weather = $weatherService->getWeather($city);
+
+echo "\n";
+echo "City: " . $weather['city'] . "\n";
+echo "Temperature: " . $weather['temperature'] . "°C\n";
+echo "Description: " . $weather['description'] . "\n";
+echo "Humidity: " . $weather['humidity'] . "%\n";
